f6a162d6b971a475e92b13e9bdc32b586aac6bab
Wonko <wonko@hanstool.org>
2014-06-03 15:32:24 +0200

    alias: o, bd

--- bashrc.d/90-aliases.bash
+++ bashrc.d/90-aliases.bash
@@ -8,6 +8,7 @@ alias nano='nano -w'
 alias ping='ping -c 5 -O '
 alias shred='shred -uz'
 
+alias bd="lsblk --output name,mountpoint,fstype,size,label,partlabel,model"
 alias l='ls --color=auto -lh --file-type'
 
 alias ln='ln -i'
@@ -19,3 +20,12 @@ alias sudo='sudo '
 alias :q=' false'
 alias :Q=' false'
 alias :x=' false'
+
+
+o(){
+	while [ -n "$1" ]
+	do
+		xdg-open "$1" 2>/dev/null >/dev/null &
+		shift;
+	done
+}

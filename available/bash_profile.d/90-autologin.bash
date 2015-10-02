0891ecbc4f94da2014dbd88bd59c736a6fb2095a
Wonko <wonko@hanstool.org>
2015-10-02 20:04:43 +0200

    move Autostart actions to external script

--- available/bash_profile.d/90-autologin.bash
+++ available/bash_profile.d/90-autologin.bash
@@ -1,25 +1,7 @@
-_do_after_login()
-{
-	if ! lsusb | grep 'G510'
-	then
-		return 0
-	fi
-
-	sleep 5
-	mpd&
-	sleep 1
-	mpc clear
-	mpc load current
-	mpc random on
-	mpc repeat on
-	mpc play
-	sleep 4
-	sudo pacman -Sy &&  pacman -Quq > ~/tmp/.pacman-packages
-}
 
 if [[ -z "$DISPLAY" &&  $XDG_VTNR -eq 1 ]]
 then
-	_do_after_login &>/dev/null &
+	autostart &>/dev/null &
 	read -t 1 -n 1 -p "StartX? [Y/n]: " check
 	if [ "$check" != "n" ] && [ "$check" != 'N' ]
 	then

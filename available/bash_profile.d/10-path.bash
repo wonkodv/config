1349a38e715fcc2dffcc5bc38c3a56afea9a1748
Wonko <wonko@hanstool.org>
2015-11-12 09:59:07 +0100

    ~/bin overwrites ~/.local/bin

--- available/bash_profile.d/10-path.bash
+++ available/bash_profile.d/10-path.bash
@@ -1,9 +1,14 @@
-if [ -d "$HOME/bin" ]
-then
-	export PATH=$HOME/bin:$PATH
-fi
+PATH="/bin:$PATH"
 
 if [ -d "$HOME/.local/bin" ]
 then
-	export PATH=$HOME/.local/bin:$PATH
+    PATH=$HOME/.local/bin:$PATH
 fi
+
+if [ -d "$HOME/bin" ]
+then
+    PATH=$HOME/bin:$PATH
+fi
+
+
+export PATH

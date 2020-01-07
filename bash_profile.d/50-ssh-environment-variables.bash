b86fa1cc31c82f29771dcf224a3e761a30104ae5
Wonko <wonko@hanstool.org>
2020-01-07 23:07:44 +0100

    dont overwrite DISPLAY if set

--- bash_profile.d/50-ssh-environment-variables.bash
+++ bash_profile.d/50-ssh-environment-variables.bash
@@ -1,7 +1,7 @@
 # If connecting from SSH, set DISPLAY= and DBUS_SESSION to the value that some other process (the user is allowed to inspect
 # has in its environment
 
-if [ -n "$SSH_CLIENT" ]
+if [[ -n "$SSH_CLIENT" && -z "$DISPLAY" ]]
 then
     export $(strings /proc/*/environ 2>/dev/null | grep '^DISPLAY\|^DBUS_SESSION' | sort -u  | tail -2)
 fi

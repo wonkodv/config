6c7bd74eb82b3616bd2edf8bff6fa66b4b208072
wonko <wonko@hanstool.org>
2022-07-01 12:57:00 +0200

    vi binfings

--- bashrc.d/15-settings.bash
+++ bashrc.d/15-settings.bash
@@ -4,6 +4,8 @@ shopt -s gnu_errfmt
 shopt -s checkjobs
 shopt -s progcomp_alias
 
+set -o vi
+
 bind "set show-all-if-ambiguous on"
 bind "set colored-completion-prefix on"
 # if above don't work try: bind "set completion-prefix-display-length 10"

b029f446bfe1206482f1b36c7455085ecba6359f
wonko <wonko@hanstool.org>
2022-07-20 13:09:16 +0200

    enable alt-. in vi-mode

--- bashrc.d/15-settings.bash
+++ bashrc.d/15-settings.bash
@@ -5,7 +5,10 @@ shopt -s checkjobs
 shopt -s progcomp_alias
 
 set -o vi
+bind '"\e.": yank-last-arg'
+bind '"\e_": yank-last-arg'
+bind '"\C-u": unix-line-discard'
+bind '"\C-w": unix-word-rubout'
 
-bind "set show-all-if-ambiguous on"
 bind "set colored-completion-prefix on"
 # if above don't work try: bind "set completion-prefix-display-length 10"

bc3132e99bfdccd69b293443ad49277bf38cd885
Wonko <wonko@hanstool.org>
2022-10-13 15:22:12 +0200

    prompt: put cmd in title, comments

--- bashrc.d/50-prompt.bash
+++ bashrc.d/50-prompt.bash
@@ -1,2 +1,6 @@
-PS0="\e[m"
-PS1='$(rc=$?; [ $rc != 0 ] && echo "\[\e[0;31m\] => $rc" && echo "\[\e[0m\]" )\[\e]0;\u@\h:\w\a\e[30;49;8m\]: \[\e[m\e[;34m\]; '
+PS0='\e[m'  # clear color for output
+PS0="$PS0"'\e]0;\u@\h:\w # $(history -a; tail -1 ~/.bash_history)\a' # put command in title
+
+PS1='$(rc=$?; [ $rc != 0 ] && echo "\[\e[0;31m\] => $rc" && echo "\[\e[0m\]" )' # colored returncode
+PS1="$PS1"'\[\e]0;\u@\h:\w\a\]' # set title
+PS1="$PS1"'\[\e[30;49;8m\]: \[\e[m\e[;34m\]; ' #  prompt

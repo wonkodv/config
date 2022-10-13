6284b65faa18bb1adce9ef9a1fdb5f688bd65085
Wonko <wonko@hanstool.org>
2022-10-13 15:23:21 +0200

    StartX: increase timeout to work around driver bugs

--- bash_profile.d/99-autologin.bash
+++ bash_profile.d/99-autologin.bash
@@ -1,8 +1,8 @@
 
 if [[ -z "$DISPLAY" &&  $XDG_VTNR -eq 1 ]]
 then
-    read -t 1 -n 1 -p "StartX? [Y/n]: " check
-    if [ "$check" != "n" ] && [ "$check" != 'N' ]
+    read -t 5 -n 1 -p "StartX? [Y/n]: " check
+    if [ "$check" == "" ] || [ "$check" == 'y' ] || [ "$check" == 'Y' ]
     then
         startx -- vt1
     fi

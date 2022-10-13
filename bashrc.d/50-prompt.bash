9fa8412ad6fdb089d5b8266689da598dd2f73a3b
Wonko <wonko@hanstool.org>
2022-10-13 19:44:32 +0200

    promprt: show command in title, comments

--- bashrc.d/50-prompt.bash
+++ bashrc.d/50-prompt.bash
@@ -1,6 +1,11 @@
-PS0='\e[m'  # clear color for output
-PS0="$PS0"'\e]0;\u@\h:\w # $(history -a; tail -1 ~/.bash_history)\a' # put command in title
+# clear color for output
+PS0='\e[m'
+# put command in title
+PS0="$PS0"'\e]0;\u@\h:\w: $(history -a; tail -1 ~/.bash_history)\a'
 
-PS1='$(rc=$?; [ $rc != 0 ] && echo "\[\e[0;31m\] => $rc" && echo "\[\e[0m\]" )' # colored returncode
-PS1="$PS1"'\[\e]0;\u@\h:\w\a\]' # set title
-PS1="$PS1"'\[\e[30;49;8m\]: \[\e[m\e[;34m\]; ' #  prompt
+# colored returncode
+PS1='$(rc=$?; [ $rc != 0 ] && echo "\[\e[0;31m\] => $rc" && echo "\[\e[0m\]" )'
+# set title
+PS1="$PS1"'\[\e]0;\u@\h:\w\a\]'
+#  prompt
+PS1="$PS1"'\[\e[30;49;8m\]: \[\e[m\e[;34m\]; '

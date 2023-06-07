7d4cfa48efbe28cc06c05b99d1550271879a67db
wonko <wonko@hanstool.org>
2023-06-07 17:07:56 +0200

    things

--- bashrc.d/20-editor.bash
+++ bashrc.d/20-editor.bash
@@ -1,26 +1,26 @@
 # disable terminal reacting to <C-s>
 stty -ixon 2>/dev/null
 
-
-alias vim='$EDITOR'
-
 if [ -n "$NVIM" ]
 then
     # once nvr checks for $NVIM, the --servername can be removed
-    export EDITOR="nvr --servername '$NVIM'"
+    export EDITOR="nvr --nostart --servername $NVIM --remote-tab-wait-silent"
+    alias vim="nvr --nostart --servername $NVIM"
     function _prompt_marker {
-        nvr --servername "$NVIM" -c ":mark p" &>/dev/null
+        nvr --nostart --servername "$NVIM" -c ":mark p" &>/dev/null
         sleep 0.05
     }
 elif [ -n "$NVIM_LISTEN_ADDRESS" ]
 then
-    export EDITOR="nvr --servername '$NVIM_LISTEN_ADDRESS'"
+    export EDITOR="nvr --nostart --servername $NVIM_LISTEN_ADDRESS --remote-tab-wait-silent"
+    alias vim="nvr --nostart --servername $NVIM_LISTEN_ADDRESS"
     function _prompt_marker {
-        nvr --servername "$NVIM_LISTEN_ADDRESS" -c ":mark p" &>/dev/null
+        nvr --nostart --servername "$NVIM_LISTEN_ADDRESS" -c ":mark p" &>/dev/null
         sleep 0.05
     }
 else
     export EDITOR=nvim
+    alias vim=nvim
     function _prompt_marker {
         true
     }
--- bashrc.d/50-prompt.bash
+++ bashrc.d/50-prompt.bash
@@ -1,9 +1,10 @@
 # _prompt_marker is defined by Editor
 PS0='\e[m$(_prompt_marker)'
+PS0='\e[m'
 # colored returncode
-PS1='$(rc=$?; [ $rc != 0 ] && echo "\[\e[0;31m\] => $rc" && echo "\[\e[0m\]" )'
+PS1='\[\e[30;49;8m\]#$(rc=$?; [ $rc != 0 ] && echo "\[\e[0;31m\]=> $rc\[\e[m\]")\[\e[0;32m\]\w\n'
 # set title
 PS1="$PS1"'\[\e]0;\u@\h:\w\a\]'
 #  prompt
-PS1="$PS1"'\[\e[30;49;8m\]: \[\e[0;32m\]\w \[\e[34m\]; '
+PS1="$PS1"'\[\e[30;49;8m\]: \[\e[0;34m\]; '
 

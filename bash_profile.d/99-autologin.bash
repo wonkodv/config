c4c4e8d771912c4973a5b562209369012796740a
Wonko <wonko@hanstool.org>
2024-02-17 11:58:28 +0100

    add gnome, wayland and reboot to win to login prompt;

--- bash_profile.d/99-autologin.bash
+++ bash_profile.d/99-autologin.bash
@@ -1,10 +1,19 @@
-
 if [[ -z "$DISPLAY" &&  $XDG_VTNR -eq 1 ]]
 then
-    read -t 5 -n 1 -p "StartX? [Y/n]: " check
-    if [ "$check" == "" ] || [ "$check" == 'y' ] || [ "$check" == 'Y' ]
-    then
-        startx -- vt1
-    fi
+    read -n 1 -t 3 -p "Start? [X with awesome / Gnome wayland / Sway wayland / reboot Windows]: " check
+    case "$check" in
+        ""|x)
+            startx -- vt1
+        ;;
+        s)
+            sway
+        ;;
+        w)
+            sudo efibootmgr -n 0001
+            reboot
+        ;;
+        g)
+            XDG_SESSION_TYPE=wayland dbus-run-session gnome-session
+        ;;
+    esac
 fi
-

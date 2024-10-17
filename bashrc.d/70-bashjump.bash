f3eb540ca6739cda7625c86c60d124daf8957a2b
Wonko <wonko@hanstool.org>
2024-10-17 15:34:19 +0200

    move jump db from .config to .local

--- bashrc.d/70-bashjump.bash
+++ bashrc.d/70-bashjump.bash
@@ -1,6 +1,6 @@
 if [ -f ~/code/bashjump/jump.bash ]
 then
-    BASHJUMP_HISTORY=~/.config/bashjump.sqlite
+    BASHJUMP_HISTORY=~/.local/bashjump.sqlite
     source ~/code/bashjump/jump.bash
     function cd(){
         bashjump_cd "$@"

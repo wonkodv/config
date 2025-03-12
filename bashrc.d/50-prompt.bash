e7a3903b299ae86834a941e97d4fff8e3a121760
wonko <wonko@hanstool.org>
2025-03-12 10:01:45 +0100

    search for all merge markers in vimmerge

--- bashrc.d/50-prompt.bash
+++ bashrc.d/50-prompt.bash
@@ -2,7 +2,7 @@
 PS0='\e[m$(_prompt_marker)'
 PS0='\e[m'
 # put command in title
-# adds full history after each command, spamming bash_history  PS0="$PS0"'\e]0;\u@\h:\w: $(history -a; tail -1 ~/.bash_history)\a'
+#PS0="$PS0"'\e]0;\u@\h:\w' # : $(history -a; tail -1 ~/.bash_history)\a' adds full history after each command, spamming bash_history, if there are multiple bashs open
 
 # colored returncode
 PS1='\[\e[30;49;8m\]# $(rc=$?; [ $rc != 0 ] && echo "\[\e[0;31m\]=> $rc\[\e[m\] ")\[\e[0;33m\]\u\[\e[0;32m\]@\[\e[0;33m\]\h:\[\e[0;32m\]\w\n'

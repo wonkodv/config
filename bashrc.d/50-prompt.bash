78cc47b6ee2851ba92e7c3734c522c3101ff302f
wonko <wonko@hanstool.org>
2025-03-27 15:28:46 +0100

    stuff

--- bashrc.d/50-prompt.bash
+++ bashrc.d/50-prompt.bash
@@ -7,6 +7,6 @@ PS0='\e[m'
 # colored returncode
 PS1='\[\e[30;49;8m\]# $(rc=$?; [ $rc != 0 ] && echo "\[\e[0;31m\]=> $rc\[\e[m\] ")\[\e[0;33m\]\u\[\e[0;32m\]@\[\e[0;33m\]\h:\[\e[0;32m\]\w\n'
 # set title
-PS1="$PS1"'\[\e]0;\u@\h:\w\a\]'
+PS1="$PS1"'\[\e]0;\w\a\]'
 #  prompt
 PS1="$PS1"'\[\e[30;49;8m\]: \[\e[0;34m\]; '
